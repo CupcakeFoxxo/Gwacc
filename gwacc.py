78bde1a0f4d578f612a68eee7f8298366694df10
Joanna Duda <joannaduda1996@gmail.com>
2020-12-09 16:25:17 -0500

    Fix readline blocking by running build output in separate thread.

--- gwacc.py
+++ gwacc.py
@@ -7,9 +7,26 @@ gameWindowSizeY = 10
 
 buildOutputSizeY = 3
 
+windowMargin = 3 # Number of lines between the two windows
+
+buildOutput = [] # List of each line from build output
+
+def readBuildOutput():
+    # Setup build process
+    buildProcess = subprocess.Popen('python -u fakeCompiler.py', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    while True:
+        buildLine = buildProcess.stdout.readline()
+        if buildLine:
+            buildOutput.append(buildLine.decode('utf-8').strip())
+        # Break if build is finished
+        if buildProcess.poll() is not None:
+            break
+
 def main(fullscreen):
     fullscreen.clear()
     fullscreen.nodelay(True) # Don't block when getting input
+    curses.curs_set(0) # Hide cursor
 
     # Divide terminal into two windows
     buildWindow = None
@@ -17,33 +34,29 @@ def main(fullscreen):
     def setupWindows(resize=False):
         (sizeY, sizeX) = fullscreen.getmaxyx()
         if resize:
-            curses.resize_term(*fullscreen.getmaxyx())
+            curses.resize_term(sizeY, sizeX)
 
         # TODO: make sure we have enough space and prompt to resize terminal if not
         buildWindow = curses.newwin(buildOutputSizeY, sizeX, 0, 0)
-        gameWindow = curses.newwin(gameWindowSizeY, gameWindowSizeX, buildOutputSizeY, 0)
+        gameWindow = curses.newwin(gameWindowSizeY, gameWindowSizeX, buildOutputSizeY + windowMargin, 0)
         return (buildWindow, gameWindow)
 
     (buildWindow, gameWindow) = setupWindows()
 
-    # Setup build process
-    buildProcess = subprocess.Popen('python fakeCompiler.py', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    buildOutput = []
+    buildThread = threading.Thread(target=readBuildOutput)
+    buildThread.start()
 
     # Main loop
-    while True:
+    while buildThread.is_alive():
+        fullscreen.refresh()
+
         # Handle game update
         inputKey = fullscreen.getch()
         if inputKey == curses.KEY_RESIZE:
-            setupWindows(resize=True)
+            (buildWindow, gameWindow) = setupWindows(resize=True)
         gameWindow.clear()
         gameWindow.addch('G')
         gameWindow.refresh()
-
-        # Handle build output
-        buildLine = buildProcess.stdout.readline()
-        if buildLine:
-            buildOutput.append(buildLine.decode('utf-8').strip())
 
         # Print last 3 lines of build output
         buildWindow.clear()
@@ -53,9 +66,7 @@ def main(fullscreen):
                 buildWindow.addstr(outputLineIndex, 0, buildOutput[buildOutputLineIndex])
         buildWindow.refresh()
 
-        # Break if build is finished
-        if buildProcess.poll() is not None:
-            break
+    curses.endwin()
 
     # Output full build
     for line in buildOutput:
